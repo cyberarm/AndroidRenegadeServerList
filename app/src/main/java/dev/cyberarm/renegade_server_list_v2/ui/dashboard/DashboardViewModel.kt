d8d14c6b892f824670e260fd992dd1a09f25e9dd
Cyberarm <matthewlikesrobots@gmail.com>
2026-01-19 11:49:50 -0600

    Initial Commit

---
+++ app/src/main/java/dev/cyberarm/renegade_server_list_v2/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package dev.cyberarm.renegade_server_list_v2.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/dev/cyberarm/renegade_server_list_v2/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package dev.cyberarm.renegade_server_list_v2.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
